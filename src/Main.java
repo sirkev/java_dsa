6e15c6c337483c5255c2e6cbd881d0f466e3f282
kev <kevinoduor8877@gmail.com>
2024-01-10 10:36:34 +0300

    HashTable

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,39 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        //Hash maps implements the Map interface
-        //they are similar to ArrayLists but with key-value pairs
-        //they store objects that need to use Wrapper Class (-concept of taking primitive data type and converting into an object)
-        //ex: (name,email), (username, userId), (country,capital)
+        //HashTable = A data structure that store unique keys to values eg: <Integer: String>
+        //            -each pair is called an Entry.
+        //            -fast insertion, lookup and deletion
+        //            -ideal for large datasets, not ideal for small data sets
 
-        HashMap<String,String> countries = new HashMap<String, String>();
+        //Hashing = takes a key and computes and integer( the hashing formula will vary based on key and data type
+        //            -in a hashtable we use the hash % capacity to calculate an index number
 
-        //add a key and value
-        countries.put("Kenya","Nairobi");
-        countries.put("Egypt","Cairo");
-        countries.put("India","New Delhi");
-        countries.put("Russia","Moscow");
+        //              key.hasCode() % capacity = index
 
-        //countries.replace("Kenya","Nakuru")
-        // System.out.println(countries.containsValue("Nairobi"));
+        //Bucket = this is an indexed storage location for one or more entries
+        //          - a bucket can store more than one entry in the case of a collision (by use of a linked list)
 
-        for(String i : countries.keySet()){
-            System.out.print(i + "\t" + "= ");
-            System.out.println(countries.get(i));
+        //Collision = this is when a hash function generates the same index for more than one key
+        //            - less collisions = more efficiency
+
+        //Runtime complexity: Best case = O(1)
+        //                    Worst case =O(n)
+
+
+        Hashtable<String, String> table = new Hashtable<>(10);
+        table.put("100","SpongeBOB");
+        table.put("123","Patrick");
+        table.put("321","Sandy");
+        table.put("555","Squidward");
+        table.put("777","Gary");
+
+       // table.remove(555);
+
+
+        for (String key : table.keySet()){
+            System.out.println(key.hashCode() % 21 +"\t" + key + "\t" + table.get(key));
         }
 
 
